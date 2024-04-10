d5a305b3155b40dbcd765484d95c829bbee50cc8
Nima <lukanima.mueller@gmail.com>
2024-04-10 14:22:49 +0200

    Student CRUD fixxed

--- src/main/java/com/studentmanager/studentmanager/student/StudentController.java
+++ src/main/java/com/studentmanager/studentmanager/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.studentmanager.studentmanager.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/student")
 public class StudentController {
 
-    StudentService studentService = new StudentService();
+    @Autowired
+    StudentService studentService;
 
     @GetMapping("{matriklNr}")
     public Student getStudent(@PathVariable int matriklNr) {
