a31ade03349f5cddd85c2ed75c6af0fbdb21c6e5
Müller <luka-nima.mueller@adesso.de>
2024-04-30 14:32:18 +0200

    sadadGE

--- src/main/java/com/studentmanager/studentmanager/student/StudentController.java
+++ src/main/java/com/studentmanager/studentmanager/student/StudentController.java
@@ -3,8 +3,6 @@ package com.studentmanager.studentmanager.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/student")
@@ -34,17 +31,6 @@ public class StudentController {
   public Student getStudent(@PathVariable int matriklNr) {
     return studentService.getStudent(matriklNr);
   }
-
-  private RestTemplate restTemplate = new RestTemplate(); 
-  @Value("${coursemanager.url}")
-  private String courseManagerUrl;
-
-  public void checkCourseId() {
-    ResponseEntity<Course> response = restTemplate.getForEntity(courseManagerUrl + "/api/v1/course/get/123", Course.class);
-    Course course = response.getBody();
-
-  }
-  
 
   // Get every Stundent in the DB.
   @GetMapping("getAll")
